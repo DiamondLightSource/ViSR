bd1405b3c730e94dc8f3022df72b94b1b2064455
root <root@pc0143.cs.diamond.ac.uk>
2024-09-20 09:13:10 +0000

    fix mirror

--- src/visr/devices/mirror.py
+++ src/visr/devices/mirror.py
@@ -1,11 +1,11 @@
+from bluesky.protocols import Readable
 from ophyd_async.device import Device
-from ophyd_async.epics import Readable
 from ophyd_async.epics.signal import epics_signal_rw
 
 
 class Mirror(Device, Readable):
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.signal = epics_signal_rw(prefix, name="signal")
+        self.signal = epics_signal_rw(float, prefix, name="signal")
         self.prefix = prefix
         super().__init__(name)
         self._mirror = None
