2000100e296d9286267a6c1f345840ac28535cee
root <root@pc0143.cs.diamond.ac.uk>
2024-09-20 09:02:38 +0000

    versions and the listening commented out

--- src/visr/devices/stage2d.py
+++ src/visr/devices/stage2d.py
@@ -1,10 +1,10 @@
-from ophyd_async.epics import Motor
+from ophyd_async.epics.motor import Motor
 from ophyd_async.epics.signal import epics_signal_rw
 
 
 class Stage2d(Motor):
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.x = epics_signal_rw(name + "_x", prefix + ":x")
-        self.y = epics_signal_rw(name + "_y", prefix + ":y")
+        self.x = epics_signal_rw(float, name + "_x", prefix + ":x")
+        self.y = epics_signal_rw(float, name + "_y", prefix + ":y")
         self.prefix = prefix
         super().__init__(name)
