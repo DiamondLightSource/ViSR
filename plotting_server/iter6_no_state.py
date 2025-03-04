db2619bb9a131abfa426d17a1d20b8c2af288d24
Stanislaw Malinowski <stanislawmalinowski09@gmail.com>
2025-03-04 18:42:21 +0000

    got the 6th iterantion now need to test with the frontend@

--- plotting_server/iter6_no_state.py
+++ plotting_server/iter6_no_state.py
@@ -4,12 +4,13 @@ import os
 from dataclasses import dataclass
 from functools import lru_cache
 from threading import Thread
-from typing import Annotated
+from typing import Annotated, Any
 
 import h5py
 import numpy as np
 import redis
-from fastapi import Depends, FastAPI, WebSocket
+from fastapi import Depends, FastAPI, HTTPException, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
@@ -29,17 +30,19 @@ state = {
 }
 
 
-def list_hdf5_tree(file_path: str) -> None:
+def list_hdf5_tree(file_path: str) -> list[Any]:
     """Recursively lists all groups and datasets in an HDF5 file."""
+    groups = []
     with h5py.File(file_path, "r") as f:
 
         def print_attrs(name, obj):
             obj_type = "Group" if isinstance(obj, h5py.Group) else "Dataset"
-            print(f"{obj_type}: {name}")
+            groups.append(f"{obj_type}: {name}")
             for key, value in obj.attrs.items():
-                print(f"  Attribute - {key}: {value}")
+                groups.append(f"  Attribute - {key}: {value}")
 
         f.visititems(print_attrs)
+    return groups
 
 
 # Example usage
@@ -191,8 +194,14 @@ class EventHandler(FileSystemEventHandler):
         self.redis = redis
         self.manager = manager
 
+    def on_created(self, event: FileSystemEvent):
+        print(event)
+
     def on_modified(self, event: FileSystemEvent):
         print(f"File modified: {event.src_path}")
+        # only react to hdf files change
+        if not event.src_path.endswith(".hdf"):
+            return
 
         # Read image data and store in Redis
         image_data = process_image(event.src_path)
@@ -203,11 +212,22 @@ class EventHandler(FileSystemEventHandler):
 
 
 app = FastAPI()
+
+# CORS setup for development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.state.redis = None
 
 
 @app.on_event("startup")
-async def startup(settings=Depends(get_settings)):  # noqa: B008
+async def startup():
+    settings = get_settings()
     app.state.redis = get_redis()
     app.state.manager = manager
 
@@ -230,12 +250,105 @@ async def get_files(
     settings: Annotated[dict, Depends(get_settings)],
 ):
     # list all the files in the path
-    files = [f for f in os.listdir(settings.hdf_path) if f.endswith(".hdf5")]
+    files = [f for f in os.listdir(settings.hdf_path) if f.endswith(".hdf")]
     return {"files": files}
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
+@app.get("/file/{id}/detail")
+async def get_groups_in_file(
+    redis: Annotated[dict, Depends(get_redis)],
+    settings: Annotated[dict, Depends(get_settings)],
+    id: str,
+):
+    # list all the groups in the file
+    file_path = os.path.join(settings.hdf_path, id)
+    groups = list_hdf5_tree(file_path)
+    return {"groups": groups}
+
+
+@app.get("/file/{id}/group/{group_name}")
+async def get_dataset_in_group(
+    redis: Annotated[dict, Depends(get_redis)],
+    settings: Annotated[dict, Depends(get_settings)],
+    id: str,
+    group_name: str,
+):
+    # list all the datasets in the group
+    file_path = os.path.join(settings.hdf_path, id)
+    with h5py.File(file_path, "r") as f:
+        datasets = list(f[group_name])
+    return {"datasets": datasets}
+
+
+@app.get("/file/{id}/group/{group_name}/dataset/{dataset_name}")
+async def get_dataset(
+    redis: Annotated[dict, Depends(get_redis)],
+    settings: Annotated[dict, Depends(get_settings)],
+    id: str,
+    group_name: str,
+    dataset_name: str,
+    latest_n_images: int = 10,
+):
+    # get the dataset
+    file_path = os.path.join(settings.hdf_path, id)
+    if file_path is None:
+        raise HTTPException(status_code=404, detail="Dataset not present")
+
+    with h5py.File(file_path, "r") as f:
+        dset = f[group_name][dataset_name]
+        raw_data = dset[-latest_n_images:]
+        stats_list = [(process_image(img)) for img in raw_data]
+        print(f"stats list: {stats_list}")
+        fractions_list = calculate_fractions(stats_list)
+        print(f"nice fractions: {fractions_list}")
+        final_list = [ImageStatsDTO.from_array(list(a)) for a in fractions_list]
+        print(f"final list: {final_list}")
+
+        print(f"stats: {stats_list}")
+        return final_list  # ✅ FastAPI will return JSON array
+
+    raise HTTPException(status_code=404, detail="Dataset not present")
+
+
+@app.post("/demo")
+async def demo(
+    redis: Annotated[dict, Depends(get_redis)],
+    settings: Annotated[dict, Depends(get_settings)],
+):
+    # first blueapi call
+    pass
+
+
+@app.get("/file/{id}/group/{group_name}/dataset/{dataset_name}/shape")
+async def get_dataset_shape(
+    redis: Annotated[dict, Depends(get_redis)],
+    settings: Annotated[dict, Depends(get_settings)],
+    id: str,
+    group_name: str,
+    dataset_name: str,
+):
+    # get the dataset shape
+    file_path = os.path.join(settings.hdf_path, id)
+    with h5py.File(file_path, "r") as f:
+        g = f[group_name]
+        print(g)
+        print(group_name)
+        print(dataset_name)
+
+        dset = g[dataset_name]
+        print(dset)
+        shape = dset.shape
+    return {"shape": shape}
+
+
+@app.websocket("/ws/{client_id}/dataset/{dataset_name}")
+async def stream_dataset(
+    settings: Annotated[dict, Depends(get_settings)],
+    redis: Annotated[dict, Depends(get_redis)],
+    client_id: str,
+    websocket: WebSocket,
+    dataset_name: str,
+):
     await manager.connect(websocket, client_id)
     try:
         while True:
@@ -243,6 +356,57 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
             print(f"Received from {client_id}: {data}")
     except Exception:
         pass
+    finally:
+        await manager.disconnect(client_id)
+
+
+@app.websocket("/ws/{client_id}/demo")
+async def demo_stream(
+    settings: Annotated[dict, Depends(get_settings)],
+    redis: Annotated[dict, Depends(get_redis)],
+    client_id: str,
+    websocket: WebSocket,
+):
+    """
+    endpoint with hardcoded read
+    """
+
+    dataset_name: str = "entry/instrument/detector/data"
+    filepath = "/dls/b01-1/data/2025/cm40661-1/bluesky"
+    filename = "-Stan-March-2025.hdf"
+    f: h5py.File = h5py.File(os.path.join(filepath, filename), "r")
+    await manager.connect(websocket, client_id)
+    data_points = f[dataset_name]
+    try:
+        while True:
+            if data_points is None or len(data_points) == 0:
+                print("❌ No data available, sending reset message...")
+                await websocket.send_json([])
+                await asyncio.sleep(1)
+                continue  # Restart loop to wait for new data
+
+            # Loop through each batch of data points
+            for i in range(1, len(data_points) + 1):
+                raw_data: np.ndarray = data_points[-i:]
+                # print(f"raw data shape: {raw_data.shape}")
+                stats_list = [(process_image(img)) for img in raw_data]
+                # print(f"stats list: {stats_list}")
+                fractions = calculate_fractions(stats_list)
+                # print(f"fractions: {fractions}")
+                try:
+                    # Send the fractions to the frontend
+                    await websocket.send_json(fractions)
+                except Exception as e:
+                    print(f"sending json error: {e}")
+
+                # Wait for a small interval before sending the next batch
+                await asyncio.sleep(1)  # You can adjust the sleep time as needed
+            # Send an empty array to indicate end of batch
+            print("✅ Sending reset message...")
+            await websocket.send_json([])
+            await asyncio.sleep(1)  # Wait before starting a new round of data
+    except Exception as e:
+        print(f"WebSocket error: {e}")
     finally:
         await manager.disconnect(client_id)
 
